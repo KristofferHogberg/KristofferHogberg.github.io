7e2dd1e7b1d1c787a24faf1e1b76075af0eebf5d
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2022-10-13 14:04:08 +0200

    re-enable auth

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { default: mongoose } = require('mongoose');
 const credentials = require('./middleware/credentials');
-// const verifyAPIKEY = require('./middleware/verifyAPIKEY');
+const verifyAPIKEY = require('./middleware/verifyAPIKEY');
 const connectDB = require('./config/dbConn');
 const express = require('express');
 const app = express();
@@ -21,33 +21,33 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
-// app.use(cookieParser());
+app.use(cookieParser());
 
-// app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-// app.use('/', require('./routes/root'));
+app.use('/', require('./routes/root'));
 
-//app.use(verifyAPIKEY);
+app.use(verifyAPIKEY);
 
 app.use('/employees', require('./routes/api/employees'));
-// app.use('/menu', require('./routes/api/menu'));
-// app.use('/watson', require('./routes/api/watson'));
+app.use('/menu', require('./routes/api/menu'));
+app.use('/watson', require('./routes/api/watson'));
 
-// app.all('*', (req, res) => {
-//     res.status(404).json({ error: '404 Not Found' });
+app.all('*', (req, res) => {
+    res.status(404).json({ error: '404 Not Found' });
     
-// });
+});
 
-// app.all('*', (req, res) => {
-//     res.status(404);
-//     if (req.accepts('html')) {
-//         res.sendFile(path.join(__dirname, 'views', '404.html'));
-//     } else if (req.accepts('json')) {
-//         res.json({ error: '404 Not Found' });
-//     } else {
-//         res.type('txt').send('404 Not Found');
-//     }
-// });
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 Not Found' });
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
+});
 
 mongoose.connection.once('open', () => {
     console.log('Connected to DB');
